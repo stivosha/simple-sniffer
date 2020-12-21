e10451583e93ce4e0096f3fe8eb03bab2c506478
Georgy <32033856+stivosha1@users.noreply.github.com>
2020-12-22 02:02:33 +0500

    added count arg

--- arg_parser.py
+++ arg_parser.py
@@ -5,4 +5,5 @@ def parse_args():
     parser = argparse.ArgumentParser(description="simple sniffer")
     parser.add_argument("-i", "--info", nargs='+', dest='filter', type=str)
     parser.add_argument("-f", "--file", dest='filename', type=str)
+    parser.add_argument("-c", "--count", dest='count', type=int)
     return parser.parse_args()
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import socket
 from printer import get_all_frames_str
+from arg_parser import parse_args
 
 
-def main():
-    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    while True:
-        raw_data, addr = conn.recvfrom(65536)
-        print(get_all_frames_str(raw_data))
+class Sniffer:
+    def __init__(self, args):
+        self.args = args
+
+    def start(self):
+        conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+        if self.args.count is not None:
+            for i in range(self.args.count):
+                raw_data, addr = conn.recvfrom(65536)
+                print(get_all_frames_str(raw_data))
+        else:
+            while True:
+                raw_data, addr = conn.recvfrom(65536)
+                print(get_all_frames_str(raw_data))
 
 
 if __name__ == '__main__':
-    main()
+    sniffer = Sniffer(parse_args())
+    sniffer.start()
